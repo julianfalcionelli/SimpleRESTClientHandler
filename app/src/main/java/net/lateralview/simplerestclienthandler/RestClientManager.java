9598ed1b7de733c65e2cc4b381dad0aaf0e9cad4
Julián <julianfalcionelli@gmail.com>
2016-02-13 21:50:15 -0300

    Some refactor on rest client manager

--- app/src/main/java/net/lateralview/simplerestclienthandler/RestClientManager.java
+++ app/src/main/java/net/lateralview/simplerestclienthandler/RestClientManager.java
@@ -3,6 +3,7 @@ package net.lateralview.simplerestclienthandler;
 import android.content.Context;
 import android.text.TextUtils;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -35,18 +36,28 @@ public class RestClientManager
 	{
 		sInstance = new RestClientManager(context);
 	}
-
+	
 	public <T> void addToRequestQueue(Request<T> req, String tag)
 	{
-		// set the default tag if tag is empty
-		req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+		//to avoid time out
+		req.setRetryPolicy(new DefaultRetryPolicy(
+				50000,
+				DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+				DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+
+		req.setTag(tag);
 		mRequestQueue.add(req);
 	}
 
-	public <T> void addToRequestQueue(Request<T> req)
+	public <T> void makeRequest(Request<T> req)
 	{
-		req.setTag(TAG);
-		mRequestQueue.add(req);
+		makeRequest(req, TAG);
+	}
+
+	public <T> void makeRequest(Request<T> req, String tag)
+	{
+		addToRequestQueue(req, tag);
 	}
 
 	public void cancelPendingRequests(Object tag)
